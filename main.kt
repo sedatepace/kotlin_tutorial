b6889f2f839f3e2b93fa249f83dc91d89c314386
swjeong <swjeong@nanumspace.com>
2024-09-12 13:27:52 +0900

    음악플레이 중지로 변경

--- main.kt
+++ main.kt
@@ -1,50 +1,51 @@
 interface State {
-    fun open(door:Door)
-    fun close(door: Door)
+    fun play(player: Player)
+    fun pause(player:Player)
 }
 
-class CloseState: State{
-    override fun open(door:Door){
-        println("열었습니다");
-        door.setState(OpenState())
+class PlayState:State{
+    override fun play(player:Player){
+        println("이미 플레이중입니다.")
     }
     
-    override fun close(door:Door){
-        println("이미 닫혀있습니다.")
-    }
-    
-}
-
-class OpenState: State{
-    override fun open(door:Door){
-        println("이미 열려있습니다.");
-    }
-    override fun close(door:Door){
-        println("문을 닫았습니다.");
-        door.setState(CloseState())
+    override fun pause(player:Player){
+        println("중지하였습니다.")
+        player.setState(PauseState())
     }
 }
 
-class Door{
-    private var state: State = CloseState()
+class PauseState:State{
+    override fun play(player:Player){
+        println("플레이 합니다.")
+        player.setState(PlayState())
+    }
+    override fun pause(player:Player){
+        println("이미 중지중입니다.")
+    }
+}
+
+class Player{
+    private var state: State = PauseState();
     
     fun setState(state:State){
         this.state = state
     }
     
-    fun open(){
-        state.open(this)
+    fun play(){
+        state.play(this)
     }
     
-    fun close(){
-        state.close(this)
+    fun pause(){
+        state.pause(this)
     }
+    
 }
 
 fun main() {
-    var door = Door()
-    door.open()
-    door.open()
-    door.close()
-    door.close()
+  var player = Player();
+    player.play()
+    player.play()
+    player.pause()
+    player.pause()
+    
 }
